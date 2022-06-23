86673cbf8730dd2f2e1a6ad7c76133c7f054e6f6
jpal91 <98477057+jpal91@users.noreply.github.com>
2022-06-23 09:33:00 -0400

    pushing to vercel

--- pages/api/all-coms.js
+++ pages/api/all-coms.js
@@ -1,6 +1,6 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from "mongodb";
 
-import { formatting } from '../../helpers/formatting'
+import { formatting } from "../../helpers/formatting";
 
 const uri = process.env.MONGO_URL;
 const client = new MongoClient(uri, { monitorCommands: true });
@@ -10,28 +10,27 @@ const col = db.collection("comments");
 //api route to get all comments
 //finds every comment and returns them
 const handler = async (req, res) => {
-    if (req.method !== 'GET') {
-        res.status(400).send('Unknown')
-    }
+  if (req.method !== "GET") {
+    res.status(400).send("Unknown");
+  }
 
-    try {
-        await client.connect()
+  try {
+    await client.connect();
 
-        const result = await col.find().toArray()
+    const result = await col.find().toArray();
 
-        //extra step to format data coming from db
-        //in db, all comments are inserted, replies are not attached to the comment
-        //replies are instead referenced by comID in the db, formatting combines them together
-        //into one object which helps when rendering components
-        const formattedData = formatting(result)
+    //extra step to format data coming from db
+    //in db, all comments are inserted, replies are not attached to the comment
+    //replies are instead referenced by comID in the db, formatting combines them together
+    //into one object which helps when rendering components
+    const formattedData = formatting(result);
 
-        res.status(200).send(formattedData)
+    res.status(200).send(formattedData);
+  } catch (error) {
+    res.status(422).send(error.message);
+  } finally {
+    await client.close();
+  }
+};
 
-    } catch (error) {
-        res.status(422).send(error.message)
-    } finally {
-        await client.close()
-    }
-}
-
-export default handler
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,34 @@
-import axios from 'axios'
-import useSWR from 'swr'
-import { useTheme, useMediaQuery } from '@mui/material'
-import Grid from '@mui/material/Grid'
+import axios from "axios";
+import useSWR from "swr";
+import { useTheme, useMediaQuery } from "@mui/material";
+import Grid from "@mui/material/Grid";
 
-import CommentsSection from '../components/CommentsSection'
-import CommentApp from '../components/CommentApp'
+import CommentsSection from "../components/CommentsSection";
+import CommentApp from "../components/CommentApp";
 
 const HomePage = () => {
-	// fetches all comments from api on loading
-	const fetcher = url => axios.get(url).then(res => res.data)
-	const { data, error } = useSWR('/api/all-coms', fetcher)
+  // fetches all comments from api on loading
+  const fetcher = (url) => axios.get(url).then((res) => res.data);
+  const { data, error } = useSWR("/api/all-coms", fetcher);
 
-	//used media query to determine window size and change layout
-	// const matches = useMediaQuery(currentTheme.breakpoints.down("lg"));
-	const currentTheme = useTheme();
-	const matches = useMediaQuery(currentTheme.breakpoints.down("lg"));
+  //used media query to determine window size and change layout
+  // const matches = useMediaQuery(currentTheme.breakpoints.down("lg"));
+  const currentTheme = useTheme();
+  const matches = useMediaQuery(currentTheme.breakpoints.down("lg"));
 
+  if (!data) {
+    return <p>Loading...</p>;
+  }
 
-	if (!data) {
-		return <p>Loading...</p>
-	}
-	
-	return (
-		<Grid
-			container
+  if (error) {
+    console.log(error);
+  }
 
-			sx={{ width: matches ? 350 : 750 }}
-		>
-			<CommentApp query={matches} comments={data} />
-		</Grid>
-	)
+  return (
+    <Grid container sx={{ width: matches ? 350 : 750 }}>
+      <CommentApp query={matches} comments={data} />
+    </Grid>
+  );
 };
 
 export default HomePage;
